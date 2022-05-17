11af4ddcb6a0aae4ce802f3ea4417297b5c6d5d9
Harmony Lab <lead@harmonylab.com>
2022-05-16 22:55:21 -0700

    XML Formatting

--- src/scratch.py
+++ src/scratch.py
@@ -1,4 +1,4 @@
-#  scratch.py. (Modified 2022-05-15, 12:26 p.m. by Praxis)
+#  scratch.py. (Modified 2022-05-16, 10:55 p.m. by Praxis)
 #  Copyright (c) 2022 Peace Robotics Studio
 #  Licensed under the MIT License.
 #  Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -8,41 +8,51 @@
 #  copies of the Software, and to permit persons to whom the Software is
 #  furnished to do so.
 
-from types import SimpleNamespace
+import xml.etree.ElementTree as ET
 
+tree = ET.parse('country_data.xml')
+# >>> with open("smiley.svg") as file:
+# ...     ET.parse(file)
+root = tree.getroot()
+print(len(root))
+print(root[1])
 
-class ChildName:
-    def __init__(self, name):
-        self.name = name
+print(f"root.tag: {root.tag}")
+print(f"root.attribute: {root.attrib}")
 
-    def __get__(self, instance, owner):
-        # instance is our parent
-        return f'I am {self.name}, my parent is {instance.name}.'
+print("Iterating over children nodes:")
+for child in root:
+    print(child.tag, child.attrib)
 
+print("Printing a specific child node:")
+# print(root[0][1].text)
 
-class ChildDiff:
-    def __init__(self, born):
-        self.born = born
+print("Iterate recursively over  full tree (all nested children):")
+# for neighbor in root.iter('neighbor'):
+#     print(neighbor.attrib)
 
-    @property
-    def diff(self):
-        return self.born - self.parent.born
+# https://realpython.com/python-xml-parser/
 
-    def age_diff(self):
-        return f'I am {self.diff} years older than {self.parent.name}.'
+for descendant in root.iter():
+    print(descendant.tag)
 
-    def __get__(self, instance, owner):
-        print("In _get_")
-        self.parent = instance  # XXX: weakref?
-        return self  # expose object to be able call age_diff() etc.
+tag_name = "{http://www.w3.org/2000/svg}ellipse"
+for descendant in root.iter(tag_name):
+    print(descendant)
 
+# Element.findall() finds only elements with a tag which are direct children of the current element
+# Element.find() finds the first child with a particular tag
+# Element.text accesses the element’s text content
+# Element.get() accesses the element’s attributes
+# for country in root.findall('country'):
+#     rank = country.find('rank').text
+#     name = country.get('name')
+#     print(name, rank)
 
-class Parent:
-    def __init__(self, name, born):
-        self.child_name = ChildName(name='Bar')
-        self.child_diff = ChildDiff(born=42)
+# https://docs.python.org/3/library/xml.etree.elementtree.html
 
-
-parent = Parent(name='Foo', born=23)
-print(parent.child_name)             # ... I am Bar, my parent is Foo.
-print(parent.child_diff.age_diff())  # ... I am 19 years older than Foo.
+element = root[0]
+print(element.tag)
+print(element.text)
+print(element.attrib)
+print(element.get("x"))
